63239401476a6f3b2aa910c979a62f9a773e7caf
Samuel-Smalich <sms003@iup.edu>
2026-04-06 15:15:38 -0400

    Hello World

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
-package github-practice-assignment-Samuel-Smalich;
+import java.util.Scanner;
 
 public class hello {
     
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+    }
 }
